7fe918022814116c5f71376314a2bd03782ed33f
Nsikan <nsikane@live.co.uk>
2017-08-07 21:59:55 +0100

    Updated test script commands & webpack test config

--- angular_apps/createAccountApp/config/webpack.test.js
+++ angular_apps/createAccountApp/config/webpack.test.js
@@ -25,7 +25,7 @@ module.exports = {
         use: [{
           loader: 'awesome-typescript-loader',
           options: {
-            configFileName: './src/tsconfig.json'
+            configFileName: './angular_apps/createAccountApp/src/tsconfig.json',
           }
         }, {
           loader: 'angular2-template-loader'
