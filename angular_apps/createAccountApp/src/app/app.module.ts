977c3aeee49a835e51b95458bf311ff442cd04f8
Nsikan <nsikane@live.co.uk>
2017-08-08 01:07:38 +0100

    Configured clarity in createaccountapp app module.

--- angular_apps/createAccountApp/src/app/app.module.ts
+++ angular_apps/createAccountApp/src/app/app.module.ts
@@ -1,11 +1,14 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent }  from './app.component';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { ClarityModule } from "clarity-angular";
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+    imports: [
+        BrowserModule,
+        ClarityModule.forRoot(),
+     ],
+     declarations: [ AppComponent ],
+     bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {    }
