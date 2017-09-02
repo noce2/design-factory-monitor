85b736b3e6ded25c3e61a123ac9d726b7a3f25f5
Nsikan <nsikane@live.co.uk>
2017-09-02 20:21:49 +0100

    Working Audit Feature

--- src/app/insight360/audit/i360-audit-list.component.ts
+++ src/app/insight360/audit/i360-audit-list.component.ts
@@ -3,6 +3,7 @@ import { DesignService } from '../shared/i360-design.service';
 import { Design } from '../../../../app_server/models/design';
 
 @Component({
+  selector: 'sg-i360-audit-list',
   templateUrl: './i360-audit-list.component.html',
   styleUrls: ['./i360-audit-list.component.css'],
   providers: [DesignService],
@@ -14,6 +15,7 @@ export class Insight360AuditListComponent implements OnInit {
   } 
   designs: Design[];
   selected: Design[] = [];
+  toApprove: Design;
 
   ngOnInit() {
     this.designs = this.designService.getDesignData();
@@ -28,6 +30,10 @@ export class Insight360AuditListComponent implements OnInit {
   }
   
   onEdit() {
-    
+    this.toApprove = this.selected[0];
   }    
+
+  onAuditDetailClose() {
+    this.toApprove = undefined;
+  }
 }
---
+++ src/app/insight360/audit/i360-audit.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './i360-audit.component.html',
+})
+
+export class Insight360AuditComponent {
+
+}
